7e991858314fc983684c76c33b4a9237220d2713
radami <imanwiz1@gmail.com>
2023-08-08 16:02:11 -0400

    4.1 and 4.2

--- components/ExportButton.tsx
+++ components/ExportButton.tsx
@@ -235,52 +235,52 @@ export default function ExportButton() {
     };
     console.log(combinedData);
 
-    // loadFile('/template.docx', (error, content) => {
-    //   if (error) {
-    //     throw error;
-    //   }
-    //   var zip = new PizZip(content);
-    //   var doc = new Docxtemplater(zip, {
-    //     paragraphLoop: true,
-    //     linebreaks: true
-    //   });
-    //   doc.setData(combinedData);
-    //   try {
-    //     // render the document (replace all tag occurences
-    //     doc.render();
-    //   } catch (error: any) {
-    //     // The error thrown here contains additional information when logged with JSON.stringify (it contains a properties object containing all suberrors).
-    //     const replaceErrors = (key: any, value: any) => {
-    //       if (value) {
-    //         return Object.getOwnPropertyNames(value).reduce(
-    //           (error: any, key: any) => {
-    //             error[key] = value[key];
-    //             return error;
-    //           },
-    //           {}
-    //         );
-    //       }
-    //       return value;
-    //     };
-    //     console.log(JSON.stringify({ error: error }, replaceErrors));
+    loadFile('/template.docx', (error, content) => {
+      if (error) {
+        throw error;
+      }
+      var zip = new PizZip(content);
+      var doc = new Docxtemplater(zip, {
+        paragraphLoop: true,
+        linebreaks: true
+      });
+      doc.setData(combinedData);
+      try {
+        // render the document (replace all tag occurences
+        doc.render();
+      } catch (error: any) {
+        // The error thrown here contains additional information when logged with JSON.stringify (it contains a properties object containing all suberrors).
+        const replaceErrors = (key: any, value: any) => {
+          if (value) {
+            return Object.getOwnPropertyNames(value).reduce(
+              (error: any, key: any) => {
+                error[key] = value[key];
+                return error;
+              },
+              {}
+            );
+          }
+          return value;
+        };
+        console.log(JSON.stringify({ error: error }, replaceErrors));
 
-    //     if (error.properties && error.properties.errors instanceof Array) {
-    //       const errorMessages = error.properties.errors
-    //         .map((error: any) => error.properties.explanation)
-    //         .join('\n');
-    //       console.log('errorMessages', errorMessages);
-    //       // errorMessages is a humanly readable message looking like this :
-    //       // 'The tag beginning with "foobar" is unopened'
-    //     }
-    //     throw error;
-    //   }
-    //   var out = doc.getZip().generate({
-    //     type: 'blob',
-    //     mimeType:
-    //       'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-    //   }); //Output the document using Data-URI
-    //   saveAs(out, 'output.docx');
-    // });
+        if (error.properties && error.properties.errors instanceof Array) {
+          const errorMessages = error.properties.errors
+            .map((error: any) => error.properties.explanation)
+            .join('\n');
+          console.log('errorMessages', errorMessages);
+          // errorMessages is a humanly readable message looking like this :
+          // 'The tag beginning with "foobar" is unopened'
+        }
+        throw error;
+      }
+      var out = doc.getZip().generate({
+        type: 'blob',
+        mimeType:
+          'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+      }); //Output the document using Data-URI
+      saveAs(out, 'output.docx');
+    });
   };
   return (
     <div>
